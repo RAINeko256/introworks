7c9c86a8be70894696cf2f8e65495b434c35bb42
RAINeko256 <reirain256@gmail.com>
2021-11-06 10:00:42 +0900

    add Random run

---
+++ pages/RandomRun.js
@@ -0,0 +1,12 @@
+import Intro from "../components/Intro"
+import Data from "../works/RandomRun.json"
+
+const RandomRun = () => {
+  return (
+    <>
+      <Intro data={Data} />
+    </>
+  )
+}
+
+export default RandomRun
